4352cf359576873f80ac9b8da9998bdf2daf18bc
Elton J <eltonjanduhy@gmail.com>
2021-06-30 22:08:41 -0300

    Iteração 3 - Desenvolvimento das telas de processos

--- 002 - IMPLEMENTACAO/app-gestao-ui/src/environments/environment.prod.ts
+++ 002 - IMPLEMENTACAO/app-gestao-ui/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  /*
   apiUrl: 'https://gestao-processos-api.herokuapp.com',
   apiDominio: 'gestao-processos-api.herokuapp.com'
-  */
+  /*
   apiUrl: 'http://localhost:8080',
   apiDominio: 'localhost:8080'
-  
+  */
+
 };
